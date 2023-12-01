34570fe82d047bb17b96ca45e9c9a0260df7df66
kurtymd <kurtimdmitruk@gmail.com>
2023-12-01 19:12:41 +0100

    core data changes

---
+++ Achiver2TrackingApp/View/SelectTaskCardView.swift
@@ -0,0 +1,18 @@
+//
+//  SelectTaskCardView.swift
+//  Achiver2TrackingApp
+//
+//  Created by Bohdan on 01.12.2023.
+//
+
+import SwiftUI
+
+struct SelectTaskCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SelectTaskCardView()
+}
