4cf41d80734fae16c79ea8ed7dde6f132e8eccff
kurtymd <kurtimdmitruk@gmail.com>
2023-12-03 13:04:10 +0100

    AddTaskView added

---
+++ Achiver2TrackingApp/View/AddTaskView.swift
@@ -0,0 +1,18 @@
+//
+//  AddTaskView.swift
+//  Achiver2TrackingApp
+//
+//  Created by Bohdan on 03.12.2023.
+//
+
+import SwiftUI
+
+struct AddTaskView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddTaskView()
+}
