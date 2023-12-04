9609a876d78e6f4e924541c52f21eebb112b5460
kurtymd <kurtimdmitruk@gmail.com>
2023-12-04 21:37:27 +0100

    custom tabbar

--- Achiver2TrackingApp/Components/Tab.swift
+++ Achiver2TrackingApp/Components/Tab.swift
@@ -1,18 +1,18 @@
 //
-//  CreateTaskView.swift
+//  Tab.swift
 //  Achiver2TrackingApp
 //
-//  Created by Bohdan on 30.11.2023.
+//  Created by Bohdan on 04.12.2023.
 //
 
 import SwiftUI
 
-struct CreateTaskView: View {
+struct Tab: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    CreateTaskView()
+    Tab()
 }
