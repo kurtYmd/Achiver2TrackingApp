6586e58414e041f5f2f9a140f50f47eb93dd9de5
kurtymd <kurtimdmitruk@gmail.com>
2023-12-06 20:12:16 +0100

    changes in cards views

---
+++ Achiver2TrackingApp/Components/TimerPie.swift
@@ -0,0 +1,18 @@
+//
+//  TimerPie.swift
+//  Achiver2TrackingApp
+//
+//  Created by Bohdan on 06.12.2023.
+//
+
+import SwiftUI
+
+struct TimerPie: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TimerPie()
+}
