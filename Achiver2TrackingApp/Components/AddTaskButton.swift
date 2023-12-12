cacfd0e8b3cd809d1bc179c053deb8ff5edc2c3d
kurtymd <kurtimdmitruk@gmail.com>
2023-12-12 22:22:32 +0100

    HomeView changes

---
+++ Achiver2TrackingApp/Components/AddTaskButton.swift
@@ -0,0 +1,18 @@
+//
+//  AddTaskButton.swift
+//  Achiver2TrackingApp
+//
+//  Created by Bohdan on 12.12.2023.
+//
+
+import SwiftUI
+
+struct AddTaskButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddTaskButton()
+}
