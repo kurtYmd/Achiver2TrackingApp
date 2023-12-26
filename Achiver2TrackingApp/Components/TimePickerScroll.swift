b915ebbc4a4d0d5ad4a986c26f483de4d8095951
kurtymd <kurtimdmitruk@gmail.com>
2023-12-26 14:57:52 +0100

    timer changing

---
+++ Achiver2TrackingApp/Components/TimePickerScroll.swift
@@ -0,0 +1,18 @@
+//
+//  TimePickerScroll.swift
+//  Achiver2TrackingApp
+//
+//  Created by Bohdan on 26.12.2023.
+//
+
+import SwiftUI
+
+struct TimePickerScroll: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TimePickerScroll()
+}
